2aee2e0e08f25a8f9ae16c5153feb63ccbfab312
JavierMartinezCasas <100429142@alumnos.uc3m.es>
2022-11-17 17:55:54 +0100

    Codigo subido

---
+++ main.py
@@ -0,0 +1,337 @@
+# Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+import numpy as np
+import pandas as pd
+from keras.applications.mobilenet import MobileNet, preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dropout, Dense, BatchNormalization, Flatten, MaxPool2D
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, Callback
+from keras.layers import Conv2D, Reshape
+from tensorflow.keras.utils import Sequence
+from keras.backend import epsilon
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+import cv2
+
+from tqdm.notebook import tqdm_notebook as tqdm
+
+import os
+
+# Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+print(os.listdir("../input"))
+
+# Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+angry = '../input/dog-emotions-prediction/images/angry/'
+sad = '../input/dog-emotions-prediction/images/sad/'
+relaxed = '../input/dog-emotions-prediction/images/relaxed/'
+happy = '../input/dog-emotions-prediction/images/happy/'
+
+angry_path = os.listdir(angry)
+sad_path = os.listdir(sad)
+relaxed_path = os.listdir(relaxed)
+happy_path = os.listdir(happy)
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+def load_img(path):
+    image = cv2.imread(path)
+    image = cv2.resize(image,(224, 224))
+    return image[...,::-1]
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+fig = plt.figure(figsize=(10, 10))
+
+for i in range(16):
+    plt.subplot(4, 4, i+1)
+    plt.imshow(load_img(angry + angry_path[i]), cmap='gray')
+    plt.suptitle("Angry Dogs",fontsize=20)
+    plt.axis('off')
+
+plt.show()
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+fig = plt.figure(figsize=(10,10))
+
+for i in range(16):
+    plt.subplot(4, 4, i+1)
+    plt.imshow(load_img(sad + sad_path[i]), cmap='gray')
+    plt.suptitle("Sad Dogs",fontsize=20)
+    plt.title(sad_path[i][:4])
+    plt.axis('off')
+
+plt.show()
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+fig = plt.figure(figsize=(10,10))
+
+for i in range(16):
+    plt.subplot(4, 4, i+1)
+    plt.imshow(load_img(relaxed + relaxed_path[i]), cmap='gray')
+    plt.suptitle("Relaxed Dogs",fontsize=20)
+    plt.title(relaxed_path[i][:4])
+    plt.axis('off')
+
+plt.show()
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+fig = plt.figure(figsize=(10,10))
+
+for i in range(16):
+    plt.subplot(4, 4, i+1)
+    plt.imshow(load_img(happy + happy_path[i]), cmap='gray')
+    plt.suptitle("Happy Dogs",fontsize=20)
+    plt.title(happy_path[i][:4])
+    plt.axis('off')
+
+plt.show()
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+dataset_path = "/kaggle/input/dog-emotions-prediction/images"
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+data_with_aug = ImageDataGenerator(horizontal_flip=True,
+                                   vertical_flip=False,
+                                   rescale=1./255,
+                                  validation_split=0.3)
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+train = data_with_aug.flow_from_directory(dataset_path,
+                                          class_mode="binary",
+                                          target_size=(96, 96),
+                                          batch_size=32,
+                                          subset="training")
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+val = data_with_aug.flow_from_directory(dataset_path,
+                                          class_mode="binary",
+                                          target_size=(96, 96),
+                                          batch_size=32,
+                                          subset="validation"
+                                          )
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+mnet = MobileNetV2(include_top = False, weights = "imagenet" ,input_shape=(96,96,3))
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+tf.keras.backend.clear_session()
+
+model = Sequential([mnet,
+                    GlobalAveragePooling2D(),
+                    Dense(512, activation = "relu"),
+                    BatchNormalization(),
+                    Dropout(0.3),
+                    Dense(128, activation = "relu"),
+                    Dropout(0.1),
+                    # Dense(32, activation = "relu"),
+                    # Dropout(0.3),
+                    Dense(4, activation = "sigmoid")])
+
+model.layers[0].trainable = False
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics="accuracy")
+
+model.summary()
+Model: "sequential"
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+def scheduler(epoch):
+    if epoch <= 2:
+        return 0.001
+    elif epoch > 2 and epoch <= 15:
+        return 0.0001
+    else:
+        return 0.00001
+
+lr_callbacks = tf.keras.callbacks.LearningRateScheduler(scheduler)
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+hist = model.fit_generator(train,
+                    epochs=20,
+                    callbacks=[lr_callbacks],
+                    validation_data=val)
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+epochs = 20
+train_loss = hist.history['loss']
+val_loss = hist.history['val_loss']
+train_acc = hist.history['accuracy']
+val_acc = hist.history['val_accuracy']
+xc = range(epochs)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+train = data_with_aug.flow_from_directory(dataset_path,
+                                          class_mode="binary",
+                                          target_size=(224, 224),
+                                          batch_size=98,
+                                          subset="training")
+
+val = data_with_aug.flow_from_directory(dataset_path,
+                                          class_mode="binary",
+                                          target_size=(224, 224),
+                                          batch_size=98,
+                                          subset="validation"
+                                          )
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+vgg16_model = tf.keras.applications.vgg16.VGG16(include_top=False, weights="imagenet", input_shape=(224,224,3))
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+vgg16_model.output[-1]
+
+# Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+# model = Sequential()
+# for layer in vgg16_model.layers[:-1]:
+#     model.add(layer)
+
+# for layer in model.layers:
+#     layer.trainable = False
+
+# model.add(Dense(2, activation='softmax'))
+
+
+model = Sequential([vgg16_model,
+                    Flatten(),
+                    #                     GlobalAveragePooling2D(),
+                    #                     Dense(512, activation = "relu"),
+                    #                     BatchNormalization(),
+                    #                     Dropout(0.3),
+                    #                     Dense(128, activation = "relu"),
+                    #                     Dropout(0.1),
+                    #                     # Dense(32, activation = "relu"),
+                    #                     # Dropout(0.3),
+                    Dense(4, activation="sigmoid")])
+
+model.layers[0].trainable = False
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics="accuracy")
+
+model.summary()
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+hist =  model.fit_generator(train,
+                    epochs=20,
+                    callbacks=[lr_callbacks],
+                    validation_data=val)
+
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+epochs = 20
+train_loss = hist.history['loss']
+val_loss = hist.history['val_loss']
+train_acc = hist.history['accuracy']
+val_acc = hist.history['val_accuracy']
+xc = range(epochs)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+#Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+#Creating an array of predicted test images
+
+predictions = model.predict_generator(val)
+
+# Code by 'Real vs Fake face detection' https://www.kaggle.com/code/debasisdotcom/real-vs-fake-face-detection
+
+val_path = "/kaggle/input/dog-emotions-prediction/images/"
+
+plt.figure(figsize=(15, 15))
+
+start_index = 250
+
+for i in range(16):
+    plt.subplot(4, 4, i + 1)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    preds = np.argmax(predictions[[start_index + i]])
+
+    gt = val.filenames[start_index + i][9:13]
+
+    if gt == "angry":
+        gt = 0
+    elif gt == 'sad':
+        gt = 1
+    elif gt == 'pleased':
+        gt = 2
+    else:
+        gt = 3
+
+    if preds != gt:
+        col = "r"
+    else:
+        col = "g"
+
+    plt.xlabel('i={}, pred={}, gt={}'.format(start_index + i, preds, gt), color=col)
+    plt.imshow(load_img(val_path + val.filenames[start_index + i]))
+    plt.tight_layout()
+
+plt.show()
