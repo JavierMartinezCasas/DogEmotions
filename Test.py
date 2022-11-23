f6833b4b3d42b7a943efbbb75d53b601913c84f3
100429119 <100429119@alumnos.uc3m.es>
2022-11-23 12:45:20 +0100

    Testeamos mis locos

---
+++ Test.py
@@ -0,0 +1,76 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+
+matplotlib.use('TkAgg')
+from keras.applications.mobilenet_v2 import MobileNetV2
+from keras.layers import Dropout, Dense, BatchNormalization, Flatten
+from keras.layers import GlobalAveragePooling2D
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+
+
+# Carga de imagenes y muestras
+def load_img(path):
+    image = cv2.imread(path)
+    image = cv2.resize(image, (224, 224))
+    return image[..., ::-1]
+
+new_model = tf.keras.models.load_model('modelo.h5')
+dataset_path = "images"
+
+data_with_aug = ImageDataGenerator(horizontal_flip=True,
+                                   vertical_flip=False,
+                                   rescale=1. / 255,
+                                   validation_split=0.3)
+
+val = data_with_aug.flow_from_directory(dataset_path,
+                                        class_mode="binary",
+                                        target_size=(224, 224),
+                                        batch_size=98,
+                                        subset="validation"
+                                        )
+
+predictions = new_model.predict(val)
+
+val_path = "images/"
+
+plt.figure(figsize=(15, 15))
+
+start_index = 250
+
+for i in range(16):
+    plt.subplot(4, 4, i + 1)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    preds = np.argmax(predictions[[start_index + i]])
+
+    gt = val.filenames[start_index + i][9:13]
+
+    if gt == "angry":
+        gt = 0
+    elif gt == 'sad':
+        gt = 1
+    elif gt == 'pleased':
+        gt = 2
+    else:
+        gt = 3
+
+    if preds != gt:
+        col = "r"
+    else:
+        col = "g"
+
+    plt.xlabel('i={}, pred={}, gt={}'.format(start_index + i, preds, gt), color=col)
+    plt.imshow(load_img(val_path + val.filenames[start_index + i]))
+    plt.tight_layout()
+
+plt.show()
